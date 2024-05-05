2332aa0ce43c4d35f8a1aa9d433dee63ae24611a
daniel <hiroshi1991@gmail.com>
2024-05-04 21:08:13 -0300

    feat: chama funcoes do modulo de backend e imprime resultado da validacao

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 from frontend import ExcelValidadorUI
+from backend import process_excel
 
 def main():
     
     ui = ExcelValidadorUI()
     ui.display_header()
+    uploaded_file = ui.upload_file()
 
+    if uploaded_file: 
+        result, errors =  process_excel(uploaded_file)
+        ui.display_results(errors)
 
 if __name__ == '__main__': 
     main()
