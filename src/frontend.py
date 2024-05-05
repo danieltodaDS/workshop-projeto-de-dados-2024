790e9e0ea56bde2fbce8af552a5a51728fbdf72b
daniel <hiroshi1991@gmail.com>
2024-05-04 21:06:52 -0300

    fix: cria funcao que imprime possiveis erros de schema no arquivo carregado

--- src/frontend.py
+++ src/frontend.py
@@ -13,5 +13,14 @@ class ExcelValidadorUI:
     def display_header(self): 
         st.title('Validador de schema excel')
 
+    def display_results (self, errors): 
+        if errors: 
+               st.error(f"Erro na validacao: {errors}") 
+            # else:    
+            #     for error in errors: 
+            #         st.error(f"Erro na validacao: {error}")
+        else: 
+            st.success("O schema do arquivo excel esta correto")
+
     def upload_file(self): 
         return st.file_uploader('Carregue seu arquivo excel aqui', type=['xlsx'])
